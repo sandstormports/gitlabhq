2a331918afed6393bcd5bc35a7899a5fca460bdb
David Renshaw <dwrenshaw@gmail.com>
2014-12-12 11:11:53 -0500

    initialize database before packaging app

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Gitlab
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.logger = Logger.new(STDERR)
+    config.eager_load = true
+
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib
                                 #{config.root}/app/models/hooks
--- lib/tasks/test.rake
+++ lib/tasks/test.rake
@@ -5,7 +5,7 @@ task :test do
   Rake::Task["gitlab:test"].invoke
 end
 
-unless Rails.env.production?
+unless (Rails.env.production? || Rails.env.init?)
   require 'coveralls/rake/task'
   Coveralls::RakeTask.new
   desc "GITLAB | Run all tests on CI with simplecov"
