b975f3c237fb24704f1128043313a3610c856353
David Renshaw <dwrenshaw@gmail.com>
2015-01-27 09:52:17 -0500

    don't keep a .secret in the repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,7 +8,7 @@ require 'securerandom'
 # no regular words or you'll be exposed to dictionary attacks.
 
 def find_secure_token
-  token_file = Rails.root.join('.secret')
+  token_file = '/var/tmp/.secret'
   if ENV.key?('SECRET_KEY_BASE')
     ENV['SECRET_KEY_BASE']
   elsif File.exist? token_file
