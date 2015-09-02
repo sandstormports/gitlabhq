7ea809bdb01a4d8e6ba02c4d5cb4ae37bd2e5576
David Renshaw <david@sandstorm.io>
2015-09-02 09:04:28 -0400

    Get migrations working.

--- db/migrate/20150620233230_add_default_otp_required_for_login_value.rb
+++ db/migrate/20150620233230_add_default_otp_required_for_login_value.rb
@@ -1,5 +1,8 @@
 class AddDefaultOtpRequiredForLoginValue < ActiveRecord::Migration
   def up
+    # Sandstorm omits the one-time password feature.
+    return
+
     execute %q{UPDATE users SET otp_required_for_login = FALSE WHERE otp_required_for_login IS NULL}
 
     change_column :users, :otp_required_for_login, :boolean, default: false, null: false
