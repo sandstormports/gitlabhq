427827d01a46c4be5b537e82df0522753bc9ab23
David Renshaw <david@sandstorm.io>
2016-05-03 10:32:45 -0400

    undelete config def

--- config/initializers/2_app.rb
+++ config/initializers/2_app.rb
@@ -1,4 +1,8 @@
 module Gitlab
+  def self.config
+    Settings
+  end
+
   VERSION = File.read(Rails.root.join("VERSION")).strip
   REVISION = "sandstorm-app" #Gitlab::Popen.popen(%W(git log --pretty=format:%h -n 1)).first.chomp
 end
